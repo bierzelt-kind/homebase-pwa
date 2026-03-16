ac6dc19383c8938f7b36b09b374c662a501291ac
bierzelt-kind <bierzelt-kind@users.noreply.github.com>
2026-03-16 09:21:13 +0100

    Fix: clean native PortCheckPlugin + registerPlugin() for Capacitor 8

--- android/app/src/main/java/com/bierzelt/homebase/MainActivity.java
+++ android/app/src/main/java/com/bierzelt/homebase/MainActivity.java
@@ -1,5 +1,12 @@
 package com.bierzelt.homebase;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(PortCheckPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
