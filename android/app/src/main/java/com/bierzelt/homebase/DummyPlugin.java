715b392f8a37f6f0c5e0c9b5ffeed68142aee707
bierzelt-kind <bierzelt-kind@users.noreply.github.com>
2026-03-16 09:53:06 +0100

    Test: minimal empty plugin to debug build failure

---
+++ android/app/src/main/java/com/bierzelt/homebase/DummyPlugin.java
@@ -0,0 +1,7 @@
+package com.bierzelt.homebase;
+
+import com.getcapacitor.Plugin;
+
+public class DummyPlugin extends Plugin {
+    // Empty minimal plugin - just testing if it compiles
+}
--- android/app/src/main/java/com/bierzelt/homebase/MainActivity.java
+++ android/app/src/main/java/com/bierzelt/homebase/MainActivity.java
@@ -1,13 +1,5 @@
 package com.bierzelt.homebase;
 
 import com.getcapacitor.BridgeActivity;
-import android.os.Bundle;
-import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        initialPlugins.add(PortCheckPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
